f3d725da12a2c1a54e209c34e4d237a0d41b5acd
yangxinyan <yangxinyan@ucredit.com>
2016-08-17 18:50:17 +0800

    hh

--- src/test/java/com/uc/redis/RedisCasLockTest.java
+++ src/test/java/com/uc/redis/RedisCasLockTest.java
@@ -3,11 +3,17 @@ package com.uc.redis;
 import java.util.List;
 
 import redis.clients.jedis.Jedis;
-import redis.clients.jedis.Response;
 import redis.clients.jedis.Transaction;
 
 import com.alibaba.fastjson.JSON;
 
+/**
+ * redis cas乐观锁
+ * RedisCasLockTest 
+ * @author yangxinyan
+ * @date 2016年8月17日 下午6:49:56
+ *
+ */
 public class RedisCasLockTest {
 
 	public static void main(String[] args) {
