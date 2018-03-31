90c54dcdb87b0afa737b13d5945a3fbcf3575a40
yangxinyan <yangxinyan@ucredit.com>
2018-03-31 13:30:37 +0800

    web fix

--- src/main/java/com/uc/renren/web/HzController.java
+++ src/main/java/com/uc/renren/web/HzController.java
@@ -1,26 +1,44 @@
 package com.uc.renren.web;
 
+import com.alibaba.fastjson.JSONObject;
 import com.uc.renren.bean.StatBean;
 import com.uc.renren.dao.TestDao;
+import com.uc.renren.util.RES_STATUS;
 import com.uc.renren.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 /**
  * Created by yangzhen on 16/12/26.
  */
-@Controller
+@RestController
 @RequestMapping("hz")
 public class HzController {
 
     @Autowired
     private TestDao dao;
 
+    @RequestMapping("get")
+    public Result<JSONObject> getUcreditAccount() {
+        Result<JSONObject> result = new Result<>(RES_STATUS.SUCCESS);
+
+        JSONObject json = new JSONObject();
+        json.put("userCreditId", "kounan2812");
+        json.put("userCreditPwd", "kounan2812");
+
+        result.setData(json);
+        
+        return result;
+    }
+
+    @RequestMapping("stat")
     public Result<List<StatBean>> getStat(String start,String end) {
         return null;
     }
+
 }
 
